a2c08ceddb10bb4c9d318b27c6371d3e9703a3f7
Echomo Xinyu <seanxinyu2@gmail.com>
2022-12-16 20:42:18 +0800

    Add unit test for methods in findAvailableClass

--- src/test/java/seedu/address/model/student/UniqueStudentListTest.java
+++ src/test/java/seedu/address/model/student/UniqueStudentListTest.java
@@ -5,10 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_TAG_INTERMEDIATE;
+import static seedu.address.model.student.UniqueStudentList.findSlotAfterClass;
+import static seedu.address.model.student.UniqueStudentList.findSlotBeforeClass;
+import static seedu.address.model.student.UniqueStudentList.findSlotBetweenClasses;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalStudents.ALICE;
 import static seedu.address.testutil.TypicalStudents.BOB;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -17,6 +22,7 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.model.student.exceptions.DuplicateStudentException;
 import seedu.address.model.student.exceptions.StudentNotFoundException;
+import seedu.address.model.timerange.TimeRange;
 import seedu.address.testutil.StudentBuilder;
 
 public class UniqueStudentListTest {
@@ -166,5 +172,130 @@ public class UniqueStudentListTest {
     public void asUnmodifiableObservableList_modifyList_throwsUnsupportedOperationException() {
         assertThrows(UnsupportedOperationException.class, ()
             -> uniqueStudentList.asUnmodifiableObservableList().remove(0));
+    }
+
+    /**
+     * Creates a student with specified name and class time.
+     */
+    private static Student studentQuickBuilder(Name name, Class aClass) {
+        return new Student(name, ALICE.getPhone(), ALICE.getNokPhone(), ALICE.getEmail(), ALICE.getAddress(), aClass,
+                ALICE.getMoneyOwed(), ALICE.getMoneyPaid(), ALICE.getRatesPerClass(), ALICE.getAdditionalNotes(),
+                ALICE.getTags(), ALICE.getMarkStatus());
+    }
+    @Test
+    public void findSlotBeforeClassTest() {
+        LocalDate date1 = LocalDate.of(2022, 12, 16);
+        LocalDate date2 = LocalDate.of(2022, 12, 17);
+        LocalDate date3 = LocalDate.of(2022, 12, 18);
+        LocalTime time1 = LocalTime.of(10, 0, 0);
+        LocalTime time2 = LocalTime.of(11, 0, 0);
+        LocalTime time3 = LocalTime.of(12, 0, 0);
+        LocalTime time4 = LocalTime.of(13, 0, 0);
+        LocalTime time5 = LocalTime.of(14, 0, 0);
+        LocalTime time6 = LocalTime.of(15, 0, 0);
+        int duration1 = 60;
+        int duration2 = 120;
+        int duration3 = 180;
+        Student s1 = studentQuickBuilder(new Name("s1"), new Class(date1, time1, time2));
+        Student s2 = studentQuickBuilder(new Name("s2"), new Class(date1, time2, time5));
+        Student s3 = studentQuickBuilder(new Name("s3"), new Class(date2, time3, time6));
+        Student s4 = studentQuickBuilder(new Name("s4"), new Class(date3, time4, time6));
+        assertEquals(
+                new Class(date1, time1, time2),
+                findSlotBeforeClass(new TimeRange(time1, time6, duration1), date1, time1, s2));
+        assertEquals(
+                null,
+                findSlotBeforeClass(new TimeRange(time2, time6, duration1), date1, time1, s2));
+        assertEquals(
+                null,
+                findSlotBeforeClass(new TimeRange(time1, time6, duration2), date1, time1, s2));
+        assertEquals(
+                null,
+                findSlotBeforeClass(new TimeRange(time1, time6, duration1), date1, time3, s2));
+        assertEquals(
+                null,
+                findSlotBeforeClass(new TimeRange(time1, time6, duration1), date1, time1, s1));
+        assertEquals(
+                new Class(date1, time3, time4),
+                findSlotBeforeClass(new TimeRange(time1, time6, duration1), date1, time3, s3));
+        assertEquals(
+                new Class(date1, time2, time3),
+                findSlotBeforeClass(new TimeRange(time2, time6, duration1), date1, time1, s3));
+        assertEquals(
+                new Class(date2, time3, time5),
+                findSlotBeforeClass(new TimeRange(time3, time6, duration2), date1, time5, s4));
+        assertEquals(
+                new Class(date2, time1, time2),
+                findSlotBeforeClass(new TimeRange(time1, time6, duration1), date1, time6, s3));
+        assertEquals(
+                null,
+                findSlotBeforeClass(new TimeRange(time3, time6, duration2), date1, time5, s3));
+    }
+
+    @Test
+    public void findSlotBetweenClassesTest() {
+        LocalDate date1 = LocalDate.of(2022, 12, 16);
+        LocalDate date2 = LocalDate.of(2022, 12, 17);
+        LocalDate date3 = LocalDate.of(2022, 12, 18);
+        LocalTime time1 = LocalTime.of(10, 0, 0);
+        LocalTime time2 = LocalTime.of(11, 0, 0);
+        LocalTime time3 = LocalTime.of(12, 0, 0);
+        LocalTime time4 = LocalTime.of(13, 0, 0);
+        LocalTime time5 = LocalTime.of(14, 0, 0);
+        LocalTime time6 = LocalTime.of(15, 0, 0);
+        int duration1 = 60;
+        int duration2 = 120;
+        int duration3 = 180;
+        Student s1 = studentQuickBuilder(new Name("s1"), new Class(date1, time1, time2));
+        Student s2 = studentQuickBuilder(new Name("s2"), new Class(date1, time4, time5));
+        Student s3 = studentQuickBuilder(new Name("s3"), new Class(date2, time3, time6));
+        Student s4 = studentQuickBuilder(new Name("s4"), new Class(date2, time3, time6));
+        Student s5 = studentQuickBuilder(new Name("s5"), new Class(date1, time2, time3));
+        Student s6 = studentQuickBuilder(new Name("s6"), new Class(date3, time2, time3));
+        assertEquals(
+                null,
+                findSlotBetweenClasses(new TimeRange(time1, time6, duration1), s1, s5));
+        assertEquals(
+                new Class(date1, time3, time5),
+                findSlotBetweenClasses(new TimeRange(time1, time6, duration2), s5, s4));
+        assertEquals(
+                new Class(date2, time1, time3),
+                findSlotBetweenClasses(new TimeRange(time1, time6, duration2), s2, s3));
+        assertEquals(
+                new Class(date1, time3, time6),
+                findSlotBetweenClasses(new TimeRange(time3, time6, duration3), s1, s4));
+        assertEquals(
+                new Class(date1, time2, time3),
+                findSlotBetweenClasses(new TimeRange(time1, time6, duration1), s1, s2));
+        assertEquals(
+                new Class(date2, time1, time4),
+                findSlotBetweenClasses(new TimeRange(time1, time6, duration3), s2, s6));
+    }
+    @Test
+    public void findSlotAfterClassTest() {
+        LocalDate date1 = LocalDate.of(2022, 12, 16);
+        LocalDate date2 = LocalDate.of(2022, 12, 17);
+        LocalDate date3 = LocalDate.of(2022, 12, 18);
+        LocalTime time1 = LocalTime.of(10, 0, 0);
+        LocalTime time2 = LocalTime.of(11, 0, 0);
+        LocalTime time3 = LocalTime.of(12, 0, 0);
+        LocalTime time4 = LocalTime.of(13, 0, 0);
+        LocalTime time5 = LocalTime.of(14, 0, 0);
+        LocalTime time6 = LocalTime.of(15, 0, 0);
+        int duration1 = 60;
+        int duration2 = 120;
+        int duration3 = 180;
+        Student s1 = studentQuickBuilder(new Name("s1"), new Class(date1, time1, time2));
+        Student s2 = studentQuickBuilder(new Name("s2"), new Class(date1, time4, time5));
+        Student s3 = studentQuickBuilder(new Name("s3"), new Class(date2, time3, time6));
+        assertEquals(
+                new Class(date1, time5, time6),
+                findSlotAfterClass(new TimeRange(time1, time6, duration1), s2));
+        assertEquals(
+                new Class(date1, time3, time6),
+                findSlotAfterClass(new TimeRange(time3, time6, duration3), s1));
+        assertEquals(
+                new Class(date3, time3, time5),
+                findSlotAfterClass(new TimeRange(time3, time6, duration2), s3));
     }
 }
